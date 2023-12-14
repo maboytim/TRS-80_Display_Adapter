27d1202c75a09c8c3fba90110079709d63ec6018
Matt Boytim <maboytim@yahoo.com>
2023-12-14 03:05:48 -0500

    Added FPGA files for M3 version

---
+++ fpga/vid80-3/src/gowin_osc/gowin_osc_tmp.v
@@ -0,0 +1,16 @@
+//Copyright (C)2014-2022 Gowin Semiconductor Corporation.
+//All rights reserved.
+//File Title: Template file for instantiation
+//GOWIN Version: V1.9.8.05
+//Part Number: GW1NR-LV9QN88PC6/I5
+//Device: GW1NR-9C
+//Created Time: Wed Jun 22 23:55:49 2022
+
+//Change the instance name and port connections to the signal names
+//--------Copy here to design--------
+
+    Gowin_OSC your_instance_name(
+        .oscout(oscout_o) //output oscout
+    );
+
+//--------Copy end-------------------
